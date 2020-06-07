cf9ea86dea63568640e501f9d16762dfbcee4e52
kz04px <6536046+kz04px@users.noreply.github.com>
2020-06-07 02:22:41 +0100

    Add piece type array

--- src/libchess/piece.hpp
+++ src/libchess/piece.hpp
@@ -1,6 +1,8 @@
 #ifndef LIBCHESS_PIECE_HPP
 #define LIBCHESS_PIECE_HPP
 
+#include <array>
+
 namespace libchess {
 
 enum Piece : int
@@ -13,6 +15,15 @@ enum Piece : int
     King,
     None,
 };
+
+inline constexpr std::array<Piece, 6> pieces = {{
+    Piece::Pawn,
+    Piece::Knight,
+    Piece::Bishop,
+    Piece::Rook,
+    Piece::Queen,
+    Piece::King,
+}};
 
 }  // namespace libchess
 

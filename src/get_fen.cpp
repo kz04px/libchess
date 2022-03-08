f075e8e8afc4a4e9edbce20c3644cf8cf4309b95
kz04px <6536046+kz04px@users.noreply.github.com>
2022-03-08 21:06:02 +0000

    Rewrite Position::get_fen()
    Much nicer.

--- src/get_fen.cpp
+++ src/get_fen.cpp
@@ -2,138 +2,99 @@
 
 namespace libchess {
 
-[[nodiscard]] std::string Position::get_fen() const noexcept {
+[[nodiscard]] std::string fen_pieces(const Position &pos) noexcept {
+    const char piece_chars[2][6] = {
+        {'P', 'N', 'B', 'R', 'Q', 'K'},
+        {'p', 'n', 'b', 'r', 'q', 'k'},
+    };
+
     std::string fen;
 
-    // Part 1 -- Piece locations
-    int empty = 0;
-    for (int i = static_cast<int>(squares::A8); i >= 0; ++i) {
-        const auto sq = Square{i};
-        const auto bb = Bitboard{sq};
+    for (int y = 7; y >= 0; --y) {
+        int num_empty = 0;
 
-        if (pieces_[Piece::Pawn] & bb) {
-            if (empty > 0) {
-                fen += std::to_string(empty);
-            }
-            empty = 0;
-            if (colours_[Side::White] & bb) {
-                fen += "P";
+        for (int x = 0; x < 8; ++x) {
+            const auto sq = Square{x, y};
+            const auto bb = Bitboard{sq};
+            const auto piece = pos.piece_on(sq);
+
+            if (piece == Piece::None) {
+                num_empty++;
             } else {
-                fen += "p";
+                // Add the number of empty squares so far
+                if (num_empty > 0) {
+                    fen += std::to_string(num_empty);
+                }
+                num_empty = 0;
+
+                const auto gg = static_cast<bool>(pos.occupancy(Side::Black) & bb);
+                fen += piece_chars[gg][piece];
             }
-        } else if (pieces_[Piece::Knight] & bb) {
-            if (empty > 0) {
-                fen += std::to_string(empty);
-            }
-            empty = 0;
-            if (colours_[Side::White] & bb) {
-                fen += "N";
-            } else {
-                fen += "n";
-            }
-        } else if (pieces_[Piece::Bishop] & bb) {
-            if (empty > 0) {
-                fen += std::to_string(empty);
-            }
-            empty = 0;
-            if (colours_[Side::White] & bb) {
-                fen += "B";
-            } else {
-                fen += "b";
-            }
-        } else if (pieces_[Piece::Rook] & bb) {
-            if (empty > 0) {
-                fen += std::to_string(empty);
-            }
-            empty = 0;
-            if (colours_[Side::White] & bb) {
-                fen += "R";
-            } else {
-                fen += "r";
-            }
-        } else if (pieces_[Piece::Queen] & bb) {
-            if (empty > 0) {
-                fen += std::to_string(empty);
-            }
-            empty = 0;
-            if (colours_[Side::White] & bb) {
-                fen += "Q";
-            } else {
-                fen += "q";
-            }
-        } else if (pieces_[Piece::King] & bb) {
-            if (empty > 0) {
-                fen += std::to_string(empty);
-            }
-            empty = 0;
-            if (colours_[Side::White] & bb) {
-                fen += "K";
-            } else {
-                fen += "k";
-            }
-        } else {
-            empty++;
         }
 
-        if (sq.file() == 7) {
-            if (empty > 0) {
-                fen += std::to_string(empty);
-            }
-            empty = 0;
-            if (i > static_cast<int>(squares::H1)) {
-                fen += "/";
-            }
-            i -= 16;
+        // Add the number of empty squares when we reach the end of the rank
+        if (num_empty > 0) {
+            fen += std::to_string(num_empty);
+        }
+
+        if (y > 0) {
+            fen += "/";
         }
     }
 
-    // Part 2 -- Side to move
-    if (turn() == Side::White) {
-        fen += " w";
-    } else {
-        fen += " b";
-    }
+    return fen;
+}
 
-    // Part 3 -- Castling permissions
+[[nodiscard]] std::string fen_side(const Position &pos) noexcept {
+    return pos.turn() == Side::White ? "w" : "b";
+}
+
+[[nodiscard]] std::string fen_castling(const Position &pos) noexcept {
     std::string part;
-    if (castling_[usKSC] == true) {
+    if (pos.can_castle(Side::White, MoveType::ksc)) {
         part += "K";
     }
-    if (castling_[usQSC] == true) {
+    if (pos.can_castle(Side::White, MoveType::qsc)) {
         part += "Q";
     }
-    if (castling_[themKSC] == true) {
+    if (pos.can_castle(Side::Black, MoveType::ksc)) {
         part += "k";
     }
-    if (castling_[themQSC] == true) {
+    if (pos.can_castle(Side::Black, MoveType::qsc)) {
         part += "q";
     }
     if (part.empty()) {
         part = "-";
     }
-    fen += " " + part;
+    return part;
+}
 
-    // Part 4 -- En passant square
-    if (ep_ == 0xFF) {
-        fen += " -";
+[[nodiscard]] std::string fen_enpassant(const Position &pos) noexcept {
+    if (pos.ep_ == 0xFF) {
+        return "-";
     } else {
-        if (turn() == Side::White) {
-            const auto bb = bitboards::Rank6 & bitboards::files[ep_];
-            const auto sq = bb.lsb();
-            fen += " " + square_strings[static_cast<int>(sq)];
-        } else {
-            const auto bb = bitboards::Rank3 & bitboards::files[ep_];
-            const auto sq = bb.lsb();
-            fen += " " + square_strings[static_cast<int>(sq)];
-        }
+        const auto rank = pos.turn() == Side::White ? bitboards::Rank6 : bitboards::Rank3;
+        const auto bb = rank & bitboards::files[pos.ep_];
+        const auto sq = bb.lsb();
+        return square_strings[static_cast<int>(sq)];
     }
+}
 
-    // Part 5 -- Halfmove clock
-    fen += " " + std::to_string(halfmove_clock_);
+[[nodiscard]] std::string fen_halfmoves(const Position &pos) noexcept {
+    return std::to_string(pos.halfmoves());
+}
 
-    // Part 6 -- Fullmove number
-    fen += " " + std::to_string(fullmove_clock_);
+[[nodiscard]] std::string fen_fullmoves(const Position &pos) noexcept {
+    return std::to_string(pos.fullmoves());
+}
 
+[[nodiscard]] std::string Position::get_fen() const noexcept {
+    std::string fen = fen_pieces(*this);
+    fen += " " + fen_side(*this);
+    fen += " " + fen_castling(*this);
+    fen += " " + fen_enpassant(*this);
+    fen += " " + fen_halfmoves(*this);
+    fen += " " + fen_fullmoves(*this);
     return fen;
 }
 
